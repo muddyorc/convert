2abcb8a73b59263853bc7267559b597316dd2906
Julio Cezar <juliocezarrc16@gmail.com>
2026-01-21 11:18:41 -0300

    validating input value

---
+++ scripts.js
@@ -0,0 +1,6 @@
+const amount = document.getElementById("amount")
+
+amount.addEventListener("input", () => {
+    const hasCharactersRegex = /\D+/g
+    amount.value = amount.value.replace(hasCharactersRegex, "")
+})
