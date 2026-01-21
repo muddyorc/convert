1496ff172f2a8b1db4289fa215c9b25e7ac39c5e
Julio Cezar <juliocezarrc16@gmail.com>
2026-01-21 11:44:53 -0300

    sorting coins

--- scripts.js
+++ scripts.js
@@ -1,6 +1,33 @@
+const USD = 5.35
+const EUR = 6.27
+const GBP = 7.18
+
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
 
 amount.addEventListener("input", () => {
     const hasCharactersRegex = /\D+/g
     amount.value = amount.value.replace(hasCharactersRegex, "")
 })
+
+form.onsubmit = () => {
+    event.preventDefault()
+
+    switch (currency.value){
+        case "USD":
+            convertCurrency(amount.value, USD, "US$")
+            break
+        case "EUR":
+            convertCurrency(amount.value, EUR, "€")
+            break
+        case "GBP":
+            convertCurrency(amount.value, GBP, "£")
+            break
+    }
+}
+
+function convertCurrency(amount, price, symbol){
+
+
+}
